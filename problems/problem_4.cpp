bdfa13e186a57fa9b19460166524740eec075af0
Kamronbek Mamaroziqov <k.mamaroziqov@newuzbekistanuniversity.uz>
2023-11-14 10:02:11 +0500

    ads

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -3,7 +3,26 @@
 
 std::string problemSolution4(const std::string &macAddress) {
     // write your code here
-
+    std::string type;
+    if (macAddress == "FF:FF:FF:FF:FF:FF") {
+        type = "broadcast";
+    } else {
+        char first = macAddress[0];
+        int first_hex;
+        if (first >= '0' && first <= '9') {
+            first_hex = first - '0';
+        } else if (first >= 'A' && first <= 'F') {
+            first_hex = first - 'A' + 10;
+        } else if (first >= 'a' && first <= 'f') {
+            first_hex = first - 'a' + 10;
+        }
+        if (0 == first_hex % 2) {
+            type = "Unicast";
+        } else {
+            type = "Multicast";
+        }
+    }
+}
     // make use of control flow statements
     // return result;
-}
+
