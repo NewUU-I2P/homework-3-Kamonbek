e9b08305ee6de366d8c4ea679c1bcdd2e2bab2cb
Kamronbek Mamaroziqov <k.mamaroziqov@newuzbekistanuniversity.uz>
2023-11-14 09:10:07 +0500

    ads

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -2,6 +2,41 @@
 
 std::string problemSolution3(float height, char S) {
     // write your code here
+            if (S == 'M' || S == 'm')
+        {
+            if (height < 1.70)
+            {
+                return "Short";
+            }
+            else if (height > 1.85)
+            {
+
+                return "Tall";
+            }
+            else
+            {
+
+                return "Average";
+            }
+        }
+        else if (S == 'F' || S == 'f')
+        {
+            if (height < 1.60)
+            {
+
+                return "Short";
+            }
+            else if (height > 1.75)
+            {
+
+                return "Tall";
+            }
+            else
+            {
+
+                return "Average";
+            }
+        }
 
     // use return to return your result
     // make use of control flow statements
